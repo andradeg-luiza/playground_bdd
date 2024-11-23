5d708b4ffd121e89f2535cde05b462688647f645
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-23 19:26:06 -0300

    refactor: I created the userLoggedIn() command to be used in future test scenarios where a single login will be required.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,3 +10,9 @@ Cypress.Commands.add('loginGeneral', () => {
     cy.get('[data-cy="password"]').type('showtime')
     cy.get('[data-cy="login-button"]').click()
 })
+
+Cypress.Commands.add('userLoggedIn', () => {
+    cy.get('[data-cy="welcome-title"]')
+        .should('be.visible')
+        .and('have.text', 'Boas vindas ao Cypress Playground')
+})
